fb3c12666e3bd08b80d548efdbce56a89a7804c3
mottc <mottcode@163.com>
2018-04-05 22:24:57 +0800

    CL-2018.4.5(1)

--- lib/utils/blob.py
+++ lib/utils/blob.py
@@ -15,12 +15,16 @@ import cv2
 
 
 def im_list_to_blob(ims):
+  ## 将图片转为网络输入格式，假设图片已准备就绪（结构化，BGR排序）
   """Convert a list of images into a network input.
 
   Assumes images are already prepared (means subtracted, BGR order, ...).
   """
+  ## 所有图片中的最大尺寸
   max_shape = np.array([im.shape for im in ims]).max(axis=0)
+  ## 图片数量
   num_images = len(ims)
+  ## 图片总数，高最大值，宽最大值，通道数
   blob = np.zeros((num_images, max_shape[0], max_shape[1], 3),
                   dtype=np.float32)
   for i in range(num_images):
@@ -32,11 +36,16 @@ def im_list_to_blob(ims):
 
 def prep_im_for_blob(im, pixel_means, target_size, max_size):
   """Mean subtract and scale an image for use in a blob."""
+  ## 转格式
   im = im.astype(np.float32, copy=False)
+  ## 减平均值
   im -= pixel_means
   im_shape = im.shape
+  ## 最小值、最大值
   im_size_min = np.min(im_shape[0:2])
   im_size_max = np.max(im_shape[0:2])
+
+  ## 把短边化为target_size大小，若此时长边已经大于max_size,则把长边化为max_size大小。长高皆为同比例。
   im_scale = float(target_size) / float(im_size_min)
   # Prevent the biggest axis from being more than MAX_SIZE
   if np.round(im_scale * im_size_max) > max_size:
